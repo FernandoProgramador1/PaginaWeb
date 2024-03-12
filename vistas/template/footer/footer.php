9a266f63fee950775de5d8464a6bb3cd82a5bf14
Isma1398 <ismael.guillermo@hotmail.com>
2024-03-12 11:41:34 -0600

    Genero footer y estilos

--- vistas/template/footer/footer.php
+++ vistas/template/footer/footer.php
@@ -2,58 +2,55 @@
 require_once('modelos/model_contactos.php');
 ?>
 
-<footer id="Contacto" class="footer-bg pt-1 pb-1 mt-5">
-    <div class="footer-container">
+<footer class="footer footer-fondo">
+    <div class="container">
         <div class="row">
-
-            <!-- Logo de la Empresa -->
-            <div class="col-lg-3 col-md-3">
-                <img src="recursos/img/LOGO-SSETCO.jpeg" alt="Logo" class="footer-logo" />
-            </div>
-
-            <!-- Menú -->
-            <div class="col-lg-3 col-md-3">
-                <h5 class="footer-title text-uppercase">Menú</h5>
-                <ul class="list-unstyled footer-menu"> <!-- Agregado una clase "footer-menu" al ul -->
-                    <li><a href="index.php" class="footer-link">Inicio</a></li>
-                    <li><a href="index.php?page=Productos" class="footer-link">Productos</a></li>
-                    <li><a href="index.php?page=Nosotros" class="footer-link">Nosotros</a></li>
-                    <!-- <li><a href="#" class="footer-link">Contacto</a></li> -->
-                </ul>
-            </div>
-            <?php
-            foreach ($dtcontactos as $row) :
-            ?>
-                <!-- Contacto directo -->
-                <div class="col-lg-3 col-md-3">
-                    <h5 class="footer-title text-uppercase">Contacto</h5>
-                    <p><a href="mailto<?php echo $row["Correo"] ?>" class="footer-link"><?php echo $row["Correo"] ?></a></p>
-                    <p><a href="tel:+<?php echo $row["Telefono"] ?>" class="footer-link">+<?php echo $row["Telefono"] ?></a></p>
-                </div>
-                <!-- Redes Sociales -->
-                <div class="col-lg-3 col-md-3">
-                    <h5 class="footer-title text-uppercase">Dirección</h5>
-                    <ul class="list-unstyled footer-menu"> <!-- Agregado una clase "footer-menu" al ul -->
-                        <li><a href="#" class="footer-link"><?php echo $row["Ciudad"] . ", " . $row["Estado"] . ", " . $row["CodigoP"] ?></a></li>
-                        <!-- <li><a href="#" class="footer-link">Nosotros</a></li>
-                    <li><a href="#" class="footer-link">Contacto</a></li> -->
+            <div class="col-sm-6 col-md-4 mt-4 col-lg-3 text-center text-sm-start">
+                <div class="information">
+                    <h6 class="footer-heading text-uppercase fw-bold">Information</h6>
+                    <ul class="list-unstyled footer-link mt-4">
+                        <li class="mb-1"><a href="#" class="fw-semibold">Events</a></li>
+                        <li class="mb-1"><a href="#" class="fw-semibold">Our Team</a></li>
+                        <li class="mb-1"><a href="#" class="fw-semibold">Upcoming Sale</a></li>
+                        <li class=""><a href="#" class="fw-semibold">New Launches</a></li>
                     </ul>
                 </div>
-            <?php
-            endforeach;
-            ?>
-
-            <?php
-            if ((!empty($dtcontactos)) && (isset($dtcontactos))) {
-                foreach ($dtcontactos as $contac) :
-            ?>
-                    <a href="https://wa.me/1<?php echo $contac["Telefono"] ?>" class="whatsapp" target="_blank"> <i class="fab fa-whatsapp footer-i whatsapp-icon"></i></a>
-            <?php
-                endforeach;
-            }
-            ?>
-
+            </div>
+            <div class="col-sm-6 col-md-4 mt-4 col-lg-3 text-center text-sm-start">
+                <div class="resources">
+                    <h6 class="footer-heading text-uppercase fw-bold">Resources</h6>
+                    <ul class="list-unstyled footer-link mt-4">
+                        <li class="mb-1"><a href="#" class="fw-semibold">API</a></li>
+                        <li class="mb-1"><a href="#" class="fw-semibold">Website Tutorials</a></li>
+                        <li class="mb-1"><a href="#" class="fw-semibold">Third Party</a></li>
+                        <li class=""><a href="#" class="fw-semibold">Video Lectures</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 mt-4 col-lg-2 text-center text-sm-start">
+                <div class="social">
+                    <h6 class="footer-heading text-uppercase text-white fw-bold">Social</h6>
+                    <ul class="list-inline my-4">
+                        <li class="list-inline-item"><a href="#" class="btn-sm btn btn-primary mb-2"><i class="bi bi-facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="text-danger btn-sm btn btn-light mb-2"><i class="bi bi-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="btn-sm btn btn-primary mb-2"><i class="bi bi-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="btn-sm btn btn-success mb-2"><i class="bi bi-whatsapp"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-6 mt-4 col-lg-4 text-center text-sm-start">
+                <div class="contact footer-link">
+                    <h6 class="footer-heading text-uppercase fw-bold">Contact Us</h6>
+                    <address class="mt-4 m-0 mb-1"><i class="bi bi-pin-map fw-semibold"></i> New South Block , Phase 8B , 160055</address>
+                    <a href="tel:+" class="mb-1 fw-semibold"><i class="bi bi-telephone"></i> 909090XXXX</a>
+                    <a href="mailto:" class="mb-1 fw-semibold"><i class="bi bi-envelope"></i> xyzdemomail@gmail.com</a>
+                    <a href="" class=" fw-semibold"><i class="bi bi-skype"></i> xyzdemomail</a>
+                </div>
+            </div>
         </div>
+    </div>
+    <div class="text-center bg-dark text-white mt-4 p-1">
+        <p class="mb-0 fw-bold">2023 © Gaurav Rana, All Rights Reserved</p>
     </div>
 </footer>
 
@@ -62,6 +59,7 @@ require_once('modelos/model_contactos.php');
 <script src="recursos/lib/jquery-validation/dist/jquery.validate.min.js"></script>
 <script src="recursos/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.min.js"></script>
 <script src="recursos/JS/site.js"></script>
+<script src="recursos/JS/nav.js"></script>
 
 <script src="recursos/lib/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> -->
