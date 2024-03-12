ee11b615e30ed959687fb7146783a81948588f55
Isma1398 <ismael.guillermo@hotmail.com>
2024-03-12 16:57:06 -0600

    Icono redes sociales

--- vistas/template/footer/footer.php
+++ vistas/template/footer/footer.php
@@ -7,7 +7,7 @@ require_once('modelos/model_contactos.php');
         <div class="row">
             <div class="col-sm-6 col-md-4 mt-4 col-lg-3 text-center text-sm-start">
                 <div class="information">
-                    <h6 class="footer-heading text-uppercase fw-bold">Information</h6>
+                    <h6 class="footer-heading text-uppercase fw-bold">Información</h6>
                     <ul class="list-unstyled footer-link mt-4">
                         <li class="mb-1"><a href="#" class="fw-semibold">Events</a></li>
                         <li class="mb-1"><a href="#" class="fw-semibold">Our Team</a></li>
@@ -18,9 +18,9 @@ require_once('modelos/model_contactos.php');
             </div>
             <div class="col-sm-6 col-md-4 mt-4 col-lg-3 text-center text-sm-start">
                 <div class="resources">
-                    <h6 class="footer-heading text-uppercase fw-bold">Resources</h6>
+                    <h6 class="footer-heading text-uppercase fw-bold">Sistemas</h6>
                     <ul class="list-unstyled footer-link mt-4">
-                        <li class="mb-1"><a href="#" class="fw-semibold">API</a></li>
+                        <li class="mb-1"><a href="#" class="fw-semibold">Lorem ipsum dolor</a></li>
                         <li class="mb-1"><a href="#" class="fw-semibold">Website Tutorials</a></li>
                         <li class="mb-1"><a href="#" class="fw-semibold">Third Party</a></li>
                         <li class=""><a href="#" class="fw-semibold">Video Lectures</a></li>
@@ -29,22 +29,20 @@ require_once('modelos/model_contactos.php');
             </div>
             <div class="col-sm-6 col-md-4 mt-4 col-lg-2 text-center text-sm-start">
                 <div class="social">
-                    <h6 class="footer-heading text-uppercase text-white fw-bold">Social</h6>
-                    <ul class="list-inline my-4">
-                        <li class="list-inline-item"><a href="#" class="btn-sm btn btn-primary mb-2"><i class="bi bi-facebook"></i></a></li>
-                        <li class="list-inline-item"><a href="#" class="text-danger btn-sm btn btn-light mb-2"><i class="bi bi-instagram"></i></a></li>
-                        <li class="list-inline-item"><a href="#" class="btn-sm btn btn-primary mb-2"><i class="bi bi-twitter"></i></a></li>
-                        <li class="list-inline-item"><a href="#" class="btn-sm btn btn-success mb-2"><i class="bi bi-whatsapp"></i></a></li>
+                    <h6 class="footer-heading text-uppercase text-white fw-bold">Redes</h6>
+                    <ul class="list-inline my-3">
+                        <li class="list-inline-item"><a href="#" class="btn-sm btn btn-primary mb-2"><i class="fab fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="text-danger btn-sm btn btn-light mb-2"><i class="fab fa-whatsapp"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="btn-sm btn btn-primary mb-2"><i class="fab fa-youtube"></i></a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-sm-6 col-md-6 mt-4 col-lg-4 text-center text-sm-start">
                 <div class="contact footer-link">
-                    <h6 class="footer-heading text-uppercase fw-bold">Contact Us</h6>
-                    <address class="mt-4 m-0 mb-1"><i class="bi bi-pin-map fw-semibold"></i> New South Block , Phase 8B , 160055</address>
-                    <a href="tel:+" class="mb-1 fw-semibold"><i class="bi bi-telephone"></i> 909090XXXX</a>
-                    <a href="mailto:" class="mb-1 fw-semibold"><i class="bi bi-envelope"></i> xyzdemomail@gmail.com</a>
-                    <a href="" class=" fw-semibold"><i class="bi bi-skype"></i> xyzdemomail</a>
+                    <h6 class="footer-heading text-uppercase fw-bold">Contáctanos</h6>
+                    <address class="mt-4 m-0 mb-1"><i class="fas fa-map-marker-alt fw-semibold"></i> New South Block , Phase 8B , 160055</address>
+                    <a href="tel:+" class="mb-1 fw-semibold"><i class="fas fa-phone"></i> 909090XXXX</a>
+                    <a href="mailto:" class="mb-1 fw-semibold"><i class="fas fa-envelope"></i> xyzdemomail@gmail.com</a>
                 </div>
             </div>
         </div>
