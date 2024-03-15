4d13e3234642061a012b281bbfbc54a712255826
Isma1398 <ismael.guillermo@hotmail.com>
2024-03-14 18:01:50 -0600

    Agregada página de sistemas para clientes

---
+++ vistas/users/client/Sistemas.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistemas</title>
+</head>
+
+<body>
+    <header>
+        <h1>Sistemas</h1>
+    </header>
+
+    <section id="sistemasSection">
+        <div class="container">
+            <h2>Nuestros Sistemas</h2>
+        </div>
+        <div id="sistemasContainer" class="container">
+            <div class="sistema">
+                <a href="#">
+                    <img src="recursos/img/ASPEL-ICONO-VERT_COI-1.webp" alt="iconoPrueba">
+                </a>
+                <p>Sistema Ejemplo</p>
+            </div>
+            <div class="sistema">
+                <a href="#">
+                    <img src="recursos\img\ASPEL-ICONO-VERT_SAE.webp" alt="iconoPrueba">
+                </a>
+                <p>Sistema Ejemplo</p>
+            </div>
+            <div class="sistema">
+                <a href="#">
+                    <img src="recursos/img/ASPEL-ICONO-VERT_COI-1.webp" alt="iconoPrueba">
+                </a>
+                <p>Sistema Ejemplo</p>
+            </div>
+            <div class="sistema">
+                <a href="#">
+                    <img src="recursos\img\ASPEL-ICONO-VERT_SAE.webp" alt="iconoPrueba">
+                </a>
+                <p>Sistema Ejemplo</p>
+            </div>
+            <div class="sistema">
+                <a href="#">
+                    <img src="recursos/img/ASPEL-ICONO-VERT_COI-1.webp" alt="iconoPrueba">
+                </a>
+                <p>Sistema Ejemplo</p>
+            </div>
+            <div class="sistema">
+                <a href="#">
+                    <img src="recursos\img\ASPEL-ICONO-VERT_SAE.webp" alt="iconoPrueba">
+                </a>
+                <p>Sistema Ejemplo</p>
+            </div>
+            <div class="sistema">
+                <a href="#">
+                    <img src="recursos/img/ASPEL-ICONO-VERT_COI-1.webp" alt="iconoPrueba">
+                </a>
+                <p>Sistema Ejemplo</p>
+            </div>
+            <div class="sistema">
+                <a href="#">
+                    <img src="recursos\img\ASPEL-ICONO-VERT_SAE.webp" alt="iconoPrueba">
+                </a>
+                <p>Sistema Ejemplo</p>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
