dd2cdb0f37167ea6c84e943521386bb1e4b04625
Isma1398 <ismael.guillermo@hotmail.com>
2024-03-19 14:21:56 -0600

    Actualizado título de la página web

--- vistas/home/home.php
+++ vistas/home/home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SSETCO - Consultoría en Higiene y Seguridad Industrial</title>
+    <title>Pagina Web</title>
 </head>
 
 <body>
