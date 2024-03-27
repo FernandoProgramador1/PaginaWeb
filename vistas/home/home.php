815ac07d559281d51ba09d88d2160cf00939692a
Isma1398 <ismael.guillermo@hotmail.com>
2024-03-27 17:36:15 -0600

    Agrego esquema de carrusel secundario

--- vistas/home/home.php
+++ vistas/home/home.php
@@ -35,6 +35,41 @@
             </button>
         </div>
     </section>
+
+    <section id="carruselSecundario" class="container-fluid">
+        <div id="carruselSoftware" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+            <div class="carousel-inner p-3">
+                <!-- Asegúrate de que cada .carousel-item contenga hasta 4 imágenes -->
+                <!-- Primer grupo de 4 elementos -->
+                <div class="carousel-item active">
+                    <div class="d-flex">
+                        <img src="recursos\img\ASPEL-ICONO-VERT_COI-1.webp" alt="Software 1">
+                        <img src="recursos\img\ASPEL-ICONO-VERT_SAE.webp" alt="Software 2">
+                        <img src="https://placehold.co/300x200" alt="Software 3">
+                        <img src="https://placehold.co/300x200" alt="Software 4">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-flex">
+                        <img src="https://placehold.co/300x200" alt="Software 1">
+                        <img src="https://placehold.co/300x200" alt="Software 2">
+                        <img src="https://placehold.co/300x200" alt="Software 3">
+                        <img src="https://placehold.co/300x200" alt="Software 4">
+                    </div>
+                </div>
+                <!-- Puedes agregar más grupos de elementos como este -->
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carruselSoftware" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carruselSoftware" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>
+
 </body>
 
 </html>
