77cbb77dba50cf61f0e77cdf768f7d6c97b9c5cf
Isma1398 <ismael.guillermo@hotmail.com>
2024-03-28 17:14:16 -0600

    Actualización de la página de Edición de Visión

--- vistas/users/admin/EdicionVision.php
+++ vistas/users/admin/EdicionVision.php
@@ -4,53 +4,27 @@
 require_once("modelos/model_vision.php");
 
 ?>
-<title>SSETCO | Editar Visión</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Navbar lateral start-->
 
-<!-- Navbar lateral end-->
+<!DOCTYPE html>
+<html lang="es">
 
-<div class="container p-5 justify-content-center bg-dark-subtle">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar Misión</title>
+</head>
 
-    <?php
-    if ((!empty($dtvisionwhere)) && (isset($dtvisionwhere))) {
-    ?>
-        <form method="post" action="index.php?page=EdicionVision&actionvis=update&IdVi=<?php echo $IdVi ?>" enctype="multipart/form-data">
-        <?php
-    } else {
-        ?>
-            <form method="post" action="index.php?page=EdicionVision&actionvis=insert" enctype="multipart/form-data">
-                <?php
-            }
+<body class="mision-body">
+    <div class="mision-container container">
+        <form method="post" action="" enctype="multipart/form-data">
+            <h3>Publicación para "Visión"</h3>
+            <div>
+                <textarea id="DescripcionMision" name="DescripcionMision" class="form-control mision-textarea" placeholder="Visión"></textarea>
+            </div>
+            <button type="submit" class="btn btn-success btn-mision">Enviar</button>
+        </form>
+    </div>
+</body>
 
-            if (isset($dtvisionwhere)) {
-                foreach ($dtvisionwhere as $rows) :
-                ?>
-                    <div class="container-sm justify-content-center rounded-1 ms-auto me-auto p-2 bg-white">
-                        <h3 class="text-center">Publicacion para "Vision"</h3>
-                        <div class="form-floating m-3">
-                            <textarea id="DescripcionVision" name="DescripcionVision" class="form-control" value="<?php echo $rows['DescripcionVision'] ?>" rows="4" placeholder="Descripcion" style="height: 300px;"><?php echo $rows['DescripcionVision'] ?></textarea>
-                            <label for="DescripcionVision">Parrafo</label>
-                        </div>
-                    </div>
-                    <div class="container ms-auto me-auto mt-4">
-                        <button type="submit" class="btn btn-success btn-lg">Enviar</button>
-                    </div>
-                <?php
-                endforeach;
-            } else if (!isset($dtvisionwhere)) {
-                ?>
-                <div class="container-sm justify-content-center rounded-1 ms-auto me-auto p-2 bg-white">
-                    <h3 class="text-center">Publicacion para "Vision"</h3>
-                    <div class="form-floating m-3">
-                        <textarea id="DescripcionVision" name="DescripcionVision" class="form-control" rows="4" placeholder="Descripcion" style="height: 300px;"></textarea>
-                        <label for="DescripcionVision">Parrafo</label>
-                    </div>
-                </div>
-                <div class="container ms-auto me-auto mt-4">
-                    <button type="submit" class="btn btn-success btn-lg">Enviar</button>
-                </div>
-            <?php
-            }
-            ?>
-            </form>
-</div>
+</html>
