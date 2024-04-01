dd78cce051bb9298ecf92f9dd616b96d7d5c8858
Isma1398 <ismael.guillermo@hotmail.com>
2024-04-01 17:38:00 -0600

    Actualización de la página de edición de imágenes del carrusel

--- vistas/users/admin/EdicionImgCarrusel.php
+++ vistas/users/admin/EdicionImgCarrusel.php
@@ -4,62 +4,38 @@
 require_once("modelos/model_carrusel.php");
 ?>
 
-<div class="container p-5 justify-content-center bg-dark-subtle">
+<!DOCTYPE html>
+<html lang="es">
 
-    <?php 
-    if((!empty($dtcarruselwhere)) && (isset($dtcarruselwhere))){
-        ?>
-            <form method="post" action="index.php?page=EdicionImgCarrusel&actioncar=update&IdK=<?php echo $IdK ?>" enctype="multipart/form-data">
-        <?php
-    }else{
-        ?>
-            <form method="post" action="index.php?page=EdicionImgCarrusel&actioncar=insert" enctype="multipart/form-data">
-        <?php
-    }
-    
-    if(isset($dtcarruselwhere)){
-        foreach($dtcarruselwhere as $rows):
-                ?>
-                    <div class="container-sm justify-content-center rounded-1 ms-auto me-auto p-2 bg-white">
-                        <h3 class="text-center">Imagen del carrusel</h3>
-                        <div class="form form-group m-3">
-                            <input id="Archivo" name="Archivo" class="form-control form-control-lg" type="file" placeholder="Inserte una imagen de logo" onchange="myimg()" required />
-                        </div>
-                    </div>
-                    <div class="card border-0 justify-content-center m-5 rounded-1 ms-1 me-4 bg-white">
-                        <div class=" img-thumbnail rounded ms-auto me-auto mt-auto mb-auto">
-                            <img id="muestra" src="" alt="Aqui se muestra la imagen seleccionada" style="max-width:400px;max-height:300px;" />
-                            </div>
-                    </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edición Imagen del Carrusel</title>
+</head>
 
-                    <div class="container ms-auto me-auto mt-4">
-                        <button type="submit" class="btn btn-success btn-lg">Enviar</button>
-                    </div>
-                <?php
-            // }
+<body>
+    <div class="container edicionCarr-container">
+        <form method="post" action="<!-- Su acción aquí -->" enctype="multipart/form-data">
+            <h3 class="edicionCarr-heading">Imagen del Carrusel</h3>
+            <div class="form-group form-groupCustom">
+                <input id="Archivo" name="Archivo" class="form-control form-control-lg customForm-control-lg" type="file" onchange="myimg()" required />
+            </div>
+            <div class="card edicionCarr-card">
+                <img id="muestra" src="" alt="Aqui se muestra la imagen seleccionada" class="img-thumbnail" style="max-width:400px; max-height:300px;" />
+            </div>
+            <button type="submit" class="btn btn-success btn-success-custom">Enviar</button>
+        </form>
+    </div>
+    <script>
+        function myimg() {
+            var input = document.getElementById('Archivo');
+            var fileReader = new FileReader();
+            fileReader.readAsDataURL(input.files[0]);
+            fileReader.onload = function(e) {
+                document.getElementById('muestra').src = e.target.result;
+            };
+        }
+    </script>
+</body>
 
-        endforeach;
-    }else if(!isset($dtcarruselwhere)){
-                ?>
-                    <div class="container-sm justify-content-center rounded-1 ms-auto me-auto p-2 bg-white">
-                        <h3 class="text-center">Imagen del carrusel</h3>
-                        <div class="form form-group m-3">
-                            <input id="Archivo" name="Archivo" class="form-control form-control-lg" type="file" placeholder="Inserte una imagen de logo" onchange="myimg()" required />
-                        </div>
-                    </div>
-                    <div class="card border-0 justify-content-center m-5 rounded-1 ms-auto me-4 bg-white">
-                        <div class=" img-thumbnail rounded ms-auto me-auto mt-auto mb-auto">
-                            <img id="muestra" src="" alt="Aqui se muestra la imagen seleccionada" style="max-width:400px;max-height:300px;" />
-                            </div>
-                    </div>
-
-                    <div class="container ms-auto me-auto mt-4">
-                        <button type="submit" class="btn btn-success btn-lg">Enviar</button>
-                    </div>
-        <?php
-                // }
-    }
-        ?>
-    </form>
-</div>
-
+</html>
