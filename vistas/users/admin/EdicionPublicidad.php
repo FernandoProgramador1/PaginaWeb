6f491bd749de3050cc98ba20a4b372f61c1f1652
Isma1398 <ismael.guillermo@hotmail.com>
2024-04-02 17:47:49 -0600

    Actualizar página de edición de publicidad

--- vistas/users/admin/EdicionPublicidad.php
+++ vistas/users/admin/EdicionPublicidad.php
@@ -4,63 +4,42 @@
 require_once("modelos/model_publicidad.php");
 ?>
 
-<title>SSETCO | Agregar Imágen de Publicidad</title>
+<!DOCTYPE html>
+<html lang="es">
 
-<div class="container p-5 justify-content-center bg-dark-subtle">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agregar Imágen de Publicidad</title>
+</head>
 
-    <?php
-    if ((!empty($dtpublicidadwhere)) && (isset($dtpublicidadwhere))) {
-    ?>
-        <form method="post" action="index.php?page=EdicionPublicidad&actioncpubli=update&IdPub=<?php echo $IdPub ?>" enctype="multipart/form-data">
-        <?php
-    } else {
-        ?>
-            <form method="post" action="index.php?page=EdicionPublicidad&actioncpubli=insert" enctype="multipart/form-data">
-                <?php
-            }
+<body>
+    <div class="container edicionCarr-container">
+        <form method="post" action="<!-- Su acción aquí -->" enctype="multipart/form-data">
+            <h3 class="edicionCarr-heading">Imagen de Publicidad</h3>
+            <div class="form-group form-groupCustom">
+                <input id="Archivo" name="Archivo" class="form-control form-control-lg customForm-control-lg" type="file" onchange="myimg()" required />
+            </div>
+            <div class="card edicionCarr-card">
+                <img id="muestra" src="" alt="Aqui se muestra la imagen seleccionada" class="img-thumbnail" style="max-width:400px; max-height:300px;" />
+            </div>
+            <div class="button-container">
+                <button type="submit" class="btn btn-success btn-success-custom">Enviar</button>
+                <a href="index.php?page=Publicidades" class="btn btn-success btn-success-custom">Volver</a>
+            </div>
 
-            if (isset($dtpublicidadwhere)) {
-                foreach ($dtpublicidadwhere as $rows) :
-                ?>
-                    <div class="container-sm justify-content-center rounded-1 ms-auto me-auto p-2 bg-white">
-                        <h3 class="text-center">Imagen de publicidad</h3>
-                        <div class="form form-group m-3">
-                            <input id="Archivo" name="Archivo" class="form-control form-control-lg" type="file" placeholder="Imagen para publicidad" onchange="myimg()" required />
-                        </div>
-                    </div>
-                    <div class="card border-0 justify-content-center m-5 rounded-1 ms-1 me-4 bg-white">
-                        <div class=" img-thumbnail rounded ms-auto me-auto mt-auto mb-auto">
-                            <img id="muestra" src="" alt="Aqui se muestra la imagen seleccionada" style="max-width:400px;max-height:300px;" />
-                        </div>
-                    </div>
+        </form>
+    </div>
+    <script>
+        function myimg() {
+            var input = document.getElementById('Archivo');
+            var fileReader = new FileReader();
+            fileReader.readAsDataURL(input.files[0]);
+            fileReader.onload = function(e) {
+                document.getElementById('muestra').src = e.target.result;
+            };
+        }
+    </script>
+</body>
 
-                    <div class="container ms-auto me-auto mt-4">
-                        <button type="submit" class="btn btn-success btn-lg">Enviar</button>
-                    </div>
-                <?php
-                // }
-
-                endforeach;
-            } else if (!isset($dtpublicidadwhere)) {
-                ?>
-                <div class="container-sm justify-content-center rounded-1 ms-auto me-auto p-2 bg-white">
-                    <h3 class="text-center">Imagen de publicidad</h3>
-                    <div class="form form-group m-3">
-                        <input id="Archivo" name="Archivo" class="form-control form-control-lg" type="file" placeholder="Imagen para publicidad" onchange="myimg()" required />
-                    </div>
-                </div>
-                <div class="card border-0 justify-content-center m-5 rounded-1 ms-auto me-4 bg-white">
-                    <div class=" img-thumbnail rounded ms-auto me-auto mt-auto mb-auto">
-                        <img id="muestra" src="" alt="Aqui se muestra la imagen seleccionada" style="max-width:400px;max-height:300px;" />
-                    </div>
-                </div>
-
-                <div class="container ms-auto me-auto mt-4">
-                    <button type="submit" class="btn btn-success btn-lg">Enviar</button>
-                </div>
-            <?php
-                // }
-            }
-            ?>
-            </form>
-</div>
+</html>
