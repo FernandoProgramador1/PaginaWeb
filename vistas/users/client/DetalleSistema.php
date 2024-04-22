fb0421d4d97d9e46db4a8359f69491e166815304
Isma1398 <ismael.guillermo@hotmail.com>
2024-04-22 16:41:55 -0600

    paso script a la vista

--- vistas/users/client/DetalleSistema.php
+++ vistas/users/client/DetalleSistema.php
@@ -156,6 +156,23 @@
         </section>
 
     </div>
+
+    <script>
+        let items = document.querySelectorAll('.carousel .carousel-item')
+
+        items.forEach((el) => {
+            const minPerSlide = 4
+            let next = el.nextElementSibling
+            for (var i = 1; i < minPerSlide; i++) {
+                if (!next) {
+                    next = items[0]
+                }
+                let cloneChild = next.cloneNode(true)
+                el.appendChild(cloneChild.children[0])
+                next = next.nextElementSibling
+            }
+        })
+    </script>
 </body>
 
 </html>
