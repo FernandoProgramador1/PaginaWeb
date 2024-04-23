0327c35535c151b60100a964a380fc072c14decc
Isma1398 <ismael.guillermo@hotmail.com>
2024-04-23 13:39:52 -0600

    agrego vista admin para agregar funciones a sistemas

---
+++ vistas/users/admin/Sistemas/Funciones.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web | Agregar Funciones</title>
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <div class="edicionProd-container container">
+        <h1 class="text-center edicionProd-heading">Agregar Funciones (Sistemas)</h1>
+        <form enctype="multipart/form-data" id="formulario">
+            <div id="inputContainer">
+                <div class="input-group function-input-group">
+                    <div class="form-floating form-group form-group-custom function-form-group-custom">
+                        <input type="text" name="funcion[]" class="form-control form-control-custom function-form-control-custom function-form" value="" required>
+                        <label class="function-label">Agrega Funciones del Producto</label>
+                    </div>
+                    <button type="button" onclick="eliminarInput(this)" class="contacto-button function-delete-btn">Eliminar</button>
+                </div>
+            </div>
+            <button type="button" onclick="agregarInput()" class="contacto-button function-btn">Agregar Más</button>
+        </form>
+    </div>
+
+    <script>
+        function agregarInput() {
+            var container = document.getElementById('inputContainer');
+            var newInputGroup = document.createElement('div');
+            newInputGroup.className = 'input-group function-input-group';
+            newInputGroup.innerHTML = `
+                <div class="form-floating form-group form-group-custom function-form-group-custom">
+                    <input type="text" name="funcion[]" class="form-control form-control-custom function-form-control-custom function-form" value="" required>
+                    <label class="function-label">Agrega Funciones del Producto</label>
+                </div>
+                <button type="button" onclick="eliminarInput(this)" class="contacto-button function-delete-btn">Eliminar</button>
+            `;
+            container.appendChild(newInputGroup);
+        }
+
+        function eliminarInput(btn) {
+            var container = document.getElementById('inputContainer');
+            var inputGroup = btn.parentElement;
+            container.removeChild(inputGroup);
+        }
+    </script>
+</body>
+
+</html>
