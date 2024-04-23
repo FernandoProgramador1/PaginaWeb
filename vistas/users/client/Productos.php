e10e16484e35a174ca3e5e0d8e6562c9dea9a0b2
Isma1398 <ismael.guillermo@hotmail.com>
2024-04-23 13:28:41 -0600

    ajuste color barra de busqueda

--- vistas/users/client/Productos.php
+++ vistas/users/client/Productos.php
@@ -23,7 +23,6 @@ $endIndex = $startIndex + $productsPerPage;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web | Catálogo de Productos</title>
-    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body class="prod-body">
