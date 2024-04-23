af0b022961f3cf90f7117d0e2e1b24ce37794a57
Isma1398 <ismael.guillermo@hotmail.com>
2024-04-23 16:30:21 -0600

    agrego descripción

--- vistas/users/admin/Sistemas/Funciones.php
+++ vistas/users/admin/Sistemas/Funciones.php
@@ -18,8 +18,14 @@
                 <div class="input-group function-input-group">
                     <div class="form-floating form-group form-group-custom function-form-group-custom">
                         <input type="text" name="funcion[]" class="form-control form-control-custom function-form-control-custom function-form" value="" required>
-                        <label class="function-label">Agrega Funciones al Sistema</label>
+                        <label class="function-label">Nombre de la Función</label>
                     </div>
+                    <!-- Agregamos el campo para la descripción de la función -->
+                    <div class="form-floating form-group form-group-custom function-form-group-custom">
+                        <textarea name="descripcion[]" class="form-control form-control-custom function-form-control-custom function-form" required></textarea>
+                        <label class="function-label">Descripción de la Función</label>
+                    </div>
+                    <!-- Fin del campo para la descripción de la función -->
                     <button type="button" onclick="eliminarInput(this)" class="contacto-button function-delete-btn">Eliminar</button>
                 </div>
             </div>
@@ -35,8 +41,14 @@
             newInputGroup.innerHTML = `
                 <div class="form-floating form-group form-group-custom function-form-group-custom">
                     <input type="text" name="funcion[]" class="form-control form-control-custom function-form-control-custom function-form" value="" required>
-                    <label class="function-label">Agrega Funciones del Producto</label>
+                    <label class="function-label">Nombre de la Función</label>
                 </div>
+                <!-- Agregamos el campo para la descripción de la función -->
+                <div class="form-floating form-group form-group-custom function-form-group-custom">
+                    <textarea name="descripcion[]" class="form-control form-control-custom function-form-control-custom function-form" required></textarea>
+                    <label class="function-label">Descripción de la Función</label>
+                </div>
+                <!-- Fin del campo para la descripción de la función -->
                 <button type="button" onclick="eliminarInput(this)" class="contacto-button function-delete-btn">Eliminar</button>
             `;
             container.appendChild(newInputGroup);
